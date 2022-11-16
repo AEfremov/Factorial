8765dc97fdc3166b94667e1c98e285714dcef840
ae <alexandr.efremoff@gmail.com>
2022-11-16 16:49:37 +0300

    Custom coroutine scope usage.

--- app/src/main/java/ru/efremov/factorialtest/MainViewModel.kt
+++ app/src/main/java/ru/efremov/factorialtest/MainViewModel.kt
@@ -1,17 +1,19 @@
 package ru.efremov.factorialtest
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
 import kotlin.concurrent.thread
 import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel : ViewModel() {
+
+//    private val coroutineScope = CoroutineScope(Dispatchers.Main + CoroutineName("My Scope"))
+    private val coroutineScope = CoroutineScope(Dispatchers.Default + CoroutineName("My Scope"))
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
@@ -24,21 +26,38 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
-        viewModelScope.launch {
+
+        coroutineScope.launch(Dispatchers.Main) {
             val number = value.toLong()
-            val result = factorial(number)
+            val result = withContext(Dispatchers.Default /* + Job() */) {
+                factorial(number)
+            }
             _state.value = Factorial(value = result)
+            Log.d("MainViewModel", coroutineContext.toString())
         }
+
+//        viewModelScope.launch {
+//            val number = value.toLong()
+//            val result = withContext(Dispatchers.Default /* + Job() */) {
+//                factorial(number)
+//            }
+//            _state.value = Factorial(value = result)
+//
+////            withContext(Dispatchers.Default) {
+////                val result = factorial(number)
+////                withContext(Dispatchers.Main) {
+////                    _state.value = Factorial(value = result)
+////                }
+////            }
+//        }
     }
 
-    private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            result.toString()
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
         }
+        return result.toString()
     }
 
 //    private suspend fun factorial(number: Long): String {
@@ -52,4 +71,9 @@ class MainViewModel : ViewModel() {
 //            }
 //        }
 //    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
 }
